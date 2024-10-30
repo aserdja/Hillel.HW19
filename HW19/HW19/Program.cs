6cda50e357996727b19eb36dee0f2ab2c6aebfec
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-30 21:55:08 +0200

    Combined Mutex and ManualResetEvent

--- HW19/HW19/Program.cs
+++ HW19/HW19/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Progarm
+{
+	private static Mutex mutex = new();
+	private static ManualResetEvent manualResetEvent = new(false);
+
+	static void Main(string[] args)
+	{
+		Thread ownerThread = new(() =>
+		{
+			Console.WriteLine($"Gas station is open!");
+			manualResetEvent.Set();
+		});
+
+		for (int i = 0; i < 5; i++)
+		{
+			Thread thread = new(() =>
+			{
+				Console.WriteLine($"Client no.{Thread.CurrentThread.ManagedThreadId} waiting for gas station to open!");
+				manualResetEvent.WaitOne();
+				UseGasStation();
+			});			
+            thread.Start();
+		}
+
+		ownerThread.Start();
+
+		Console.ReadKey();
+	}
+
+	static void UseGasStation()
+	{
+		mutex.WaitOne();
+        Console.WriteLine($"Client no.{Thread.CurrentThread.ManagedThreadId} started using gas station!");
+		Thread.Sleep(4000);
+        Console.WriteLine($"Client no.{Thread.CurrentThread.ManagedThreadId} vacated the gas station!");
+		mutex.ReleaseMutex();
+    }
+}
